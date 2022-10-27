e9554cb39e76537a93176d78a9d721c44a76a613
Lek Peng <lekpeng96@hotmail.com>
2022-10-28 02:26:49 +0800

    feat: index merchants route

--- controllers/user/profile_controller.js
+++ controllers/user/profile_controller.js
@@ -1,11 +1,27 @@
 const models = require("../../models");
 
 const controller = {
+  index: async (req, res) => {
+    const userType = req.params.userType;
+    try {
+      const users = await models[userType].findAll({
+        attributs: {
+          exclude: ["passwordHash", "address", "phoneNumber"],
+        },
+      });
+      return res.status(200).json({ [userType]: users });
+    } catch (err) {
+      return res.status(500).json({ error: err });
+    }
+  },
   show: async (req, res) => {
     const { email, userType } = req.params;
     try {
       const userProfile = await models[userType].findOne({
         where: { email },
+        attributes: {
+          exclude: ["passwordHash"],
+        },
       });
       if (!userProfile) {
         return res.status(404).json({ error: "User not found" });
--- routers/user_routes.js
+++ routers/user_routes.js
@@ -5,12 +5,15 @@ const router = require("express").Router();
 const verifyAuthJwt = require("../middleware/verify_auth_jwt");
 const verifyUserType = require("../middleware/verify_user_type");
 
+// Auth
 router.post("/register", userController.register);
 router.patch("/confirm", userController.confirm);
 router.post("/login", userController.login);
 router.delete("/logout", userController.logout);
 router.post("/refresh-token", tokenController.refreshAccessToken);
 
+// Profiles
+router.get("/profiles/:userType", profileController.index);
 router.get("/profiles/:userType/:email", profileController.show);
 
 router.get(
