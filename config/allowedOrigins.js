8d354b463beb5427c72704e8702bdf601e88936d
Lek Peng <lekpeng96@hotmail.com>
2022-11-01 02:18:39 +0800

    fix: fix front end origin url

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://localhost:3000", "https://63600c176c10bc09673a0271--groceries-galore-frontend.netlify.app"];
+const allowedOrigins = ["http://localhost:3000", "https://groceries-galore-frontend.netlify.app"];
 
 module.exports = allowedOrigins;
