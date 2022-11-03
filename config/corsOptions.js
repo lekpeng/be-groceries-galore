0e1dd80c973cf5930aeddfc27ed79e320101f284
Lek Peng <lekpeng96@hotmail.com>
2022-11-03 21:13:43 +0800

    fix: change cors origin

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,7 +1,14 @@
 const allowedOrigins = require("./allowedOrigins");
 
 const corsOptions = {
-  origin: allowedOrigins,
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      console.log("--->ORIGIN NOT ALLOWED BY CORS<----", origin);
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   optionsSuccessStatus: 200,
   credentials: true,
   cookie: {
