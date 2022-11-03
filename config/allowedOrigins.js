cf35e3c0d93574549ce4814784a3bd12f04044ef
Lek Peng <lekpeng96@hotmail.com>
2022-11-03 20:43:48 +0800

    fix: add origin for http

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://localhost:3000", "https://groceries-galore-frontend.netlify.app"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://groceries-galore-frontend.netlify.app",
+  "https://groceries-galore-frontend.netlify.app",
+];
 
 module.exports = allowedOrigins;
