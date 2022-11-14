172db950e654800ece1f5001df04630df22929ba
Lek Peng <lekpeng96@hotmail.com>
2022-11-14 18:24:25 +0800

    fix: update front end url in allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,3 @@
-const allowedOrigins = [
-  "http://localhost:3000",
-  "http://groceries-galore-frontend.netlify.app",
-  "https://groceries-galore-frontend.netlify.app",
-];
+const allowedOrigins = ["http://localhost:3000", "http://groceries-galore.netlify.app", "https://groceries-galore.netlify.app"];
 
 module.exports = allowedOrigins;
